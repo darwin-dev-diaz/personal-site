0e2a487109d717c8f82874adbf146d034c925596
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-01-31 11:29:36 -0700

    Create pairUnorderedList function

---
+++ textBotScript.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+fs.readFile("test.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  //break up data by line breaks
+  let test = data.split("\n").filter((entry) => entry.trim());
+  console.log(pairUnorderedList(test));
+});
+
+function pairUnorderedList(arr) {
+  const list = [];
+  let ulFound = false;
+  let ulStart = null;
+  let ulEnd = null;
+
+  for (const [i, element] of arr.entries()) {
+    if (element.trim().toLowerCase() === "ul" && !ulFound) {
+      ulFound = true;
+      ulStart = i;
+    } else if (element.trim().toLowerCase() === "ul" && ulFound) {
+      ulFound = false;
+      ulEnd = i;
+    }
+
+    if (ulFound && element.trim().toLowerCase() !== "ul") {
+      list.push(element);
+    }
+  }
+
+  if (list.length === 0) return 'null';
+  else if(list) return { ulStart, ulEnd, list };
+}
